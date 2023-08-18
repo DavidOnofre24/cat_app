dc5e9c5cb92cce7aadab49d51879d07d08f0085d
David Onofre <davidonofre1010@gmail.com>
2023-08-18 14:50:49 -0500

    add breed api

---
+++ lib/api/breed_api.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:cat_app/models/breed.dart';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+
+class BreedApi {
+  final Client client;
+  BreedApi({
+    required this.client,
+  });
+
+  Future<List<Breed>> fetchCharacters(int pageNumber) async {
+    final uri = Uri.parse("https://api.thecatapi.com/v1/breeds");
+    final response = await client.get(uri, headers: {
+      "Content-Type": "application/json",
+      "x-api-key":
+          "live_rDOTcH0uwiZt6T3jRvZPYFyxJrvpkWAuAq9i09JX1athYBl6FfJI5Taaas29p9aD",
+    });
+    if (response.statusCode == 200) {
+      return breedFromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+}
